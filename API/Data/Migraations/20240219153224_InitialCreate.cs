c309d86530c3431431a64c6a3a29b2df6c8188f5
Selman <selman.fettahoglu@genesys.com>
2024-02-19 10:51:21 -0600

    end of section 2

---
+++ API/Data/Migraations/20240219153224_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migraations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuantityInStocl",
+                table: "Products");
+
+            migrationBuilder.AddColumn<int>(
+                name: "QuantityInStock",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuantityInStock",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "QuantityInStocl",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
